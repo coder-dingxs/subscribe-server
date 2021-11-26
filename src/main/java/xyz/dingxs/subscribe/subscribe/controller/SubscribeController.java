4cd11bc6ba4c4f0d63a7dd9a51c85fc10b7b4c4f
dingxs <dingronaldo@outlook.com>
2021-11-26 15:36:37 +0800

    feat：删除多余接口

--- src/main/java/xyz/dingxs/subscribe/subscribe/controller/SubscribeController.java
+++ src/main/java/xyz/dingxs/subscribe/subscribe/controller/SubscribeController.java
@@ -31,20 +31,6 @@ public class SubscribeController {
         return subscribeService.get();
     }
 
-    @ApiOperation(value = "设置订阅")
-    @ApiImplicitParam(name = "token", dataType = "java.lang.String", paramType = "header", required = true)
-    @PostMapping("/set")
-    public ResponseEntity<Boolean> set(@RequestBody @Valid SubscribeSetReq subscribeSetReq) {
-        return ResponseEntity.ok(subscribeService.set(subscribeSetReq.getUrl()));
-    }
-
-    @ApiOperation(value = "获取订阅配置")
-    @ApiImplicitParam(name = "token", dataType = "java.lang.String", paramType = "header", required = true)
-    @PostMapping("/getConfig")
-    public ResponseEntity<String> getConfig() {
-        return ResponseEntity.ok(subscribeService.getConfig());
-    }
-
     @ApiOperation(value = "设置新端口")
     @ApiImplicitParam(name = "token", dataType = "java.lang.String", paramType = "header", required = true)
     @PostMapping("/updatePort")
