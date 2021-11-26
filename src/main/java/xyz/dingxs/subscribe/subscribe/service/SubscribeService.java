a9a1112216e26799f49856996038c912853412f1
dingxs <dingronaldo@outlook.com>
2021-11-26 15:55:25 +0800

    feat：删除多余代码

--- src/main/java/xyz/dingxs/subscribe/subscribe/service/SubscribeService.java
+++ src/main/java/xyz/dingxs/subscribe/subscribe/service/SubscribeService.java
@@ -6,7 +6,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ObjectUtils;
 import xyz.dingxs.subscribe.common.constant.RedisConstant;
 import xyz.dingxs.subscribe.subscribe.dto.SubscribeDto;
 
@@ -40,76 +39,12 @@ public class SubscribeService {
     }
 
     /**
-     * set url
-     *
-     * @param url url
-     * @return 是否成功
-     */
-    public Boolean set(String url) {
-        // 解析
-        SubscribeDto subscribeDto = this.parse(url);
-        // 校验url
-        if (this.check(subscribeDto)) {
-
-            String json = this.parseJson(subscribeDto);
-            byte[] encode = Base64.getEncoder().encode(url.getBytes(StandardCharsets.UTF_8));
-            // 存两个
-            stringRedisTemplate.opsForValue().set(RedisConstant.SUBSCRIBE_URL, new String(encode));
-            assert json != null;
-            stringRedisTemplate.opsForValue().set(RedisConstant.SUBSCRIBE_DTO, json);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    /**
      * 从redis取出直接返回
      *
      * @return json
      */
     public String getConfig() {
         return stringRedisTemplate.opsForValue().get(RedisConstant.SUBSCRIBE_DTO);
-    }
-
-    /**
-     * 校验subscribeDto
-     *
-     * @param subscribeDto subscribeDto
-     * @return 校验结果
-     */
-    private Boolean check(SubscribeDto subscribeDto) {
-        if (ObjectUtils.isEmpty(subscribeDto.getAdd())) {
-            return false;
-        }
-        if (ObjectUtils.isEmpty(subscribeDto.getPort())) {
-            return false;
-        }
-        if (ObjectUtils.isEmpty(subscribeDto.getId())) {
-            return false;
-        }
-        if (ObjectUtils.isEmpty(subscribeDto.getAid())) {
-            return false;
-        }
-        return !ObjectUtils.isEmpty(subscribeDto.getNet());
-    }
-
-    /**
-     * 解析url到java-dto
-     *
-     * @param url url
-     * @return dto
-     */
-    private SubscribeDto parse(String url) {
-        SubscribeDto subscribeDto = null;
-        try {
-            url = url.substring(8);
-            byte[] urlByteArray = Base64.getDecoder().decode(url);
-            subscribeDto = objectMapper.readValue(urlByteArray, SubscribeDto.class);
-        } catch (Exception e) {
-            logger.error("url parse dto exception", e);
-        }
-        return subscribeDto;
     }
 
     /**
